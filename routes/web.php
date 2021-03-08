f964d307136c5bfb5c64f5d1789f6ca0418c4f46
syamsulhidatu <syamsulhidatullah522@gmail.com>
2021-03-08 15:16:22 +0700

    Add adminLTE

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/dashboard', function(){
+    return view ('dashboard');
+});
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
